9622673cd9db302931ca6ab3cd702a8cacbd52f3
Mahkii900 <col.graff.battleschool@gmail.com>
2019-08-15 16:29:30 -0600

    finished working on main dashboard information display

--- src/components/Rooms/Room.js
+++ src/components/Rooms/Room.js
@@ -11,12 +11,16 @@ export default class Room extends Component {
             this.setState({devices: res.data})
         })
     }
-    
+
     render() {
+        let devices = this.state.devices.map((ele) => <div key={ele.device_id}>{ele.name} {ele.type}</div>)
         return (
             <div>
                 <div>{this.props.room.name}</div>
-                <div></div>
+                <div>
+                    <button>Go to {this.props.room.name} view</button>
+                </div>
+                <div>{devices}</div>
             </div>
         )
     }
