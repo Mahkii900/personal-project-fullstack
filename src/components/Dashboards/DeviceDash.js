b9df2db90aa64e73ccf4ea0ed86666e754937ae9
Mahkii900 <col.graff.battleschool@gmail.com>
2019-08-21 15:58:43 -0600

    added deleting device to front-end

--- src/components/Dashboards/DeviceDash.js
+++ src/components/Dashboards/DeviceDash.js
@@ -8,15 +8,32 @@ export default class DeviceDash extends Component {
         devices: []
     }
 
-    componentDidMount() {
+    getDevices() {
         axios.get('/devices').then(res => {
             this.setState({devices: res.data})
         })
     }
 
+    componentDidMount() {
+        this.getDevices()
+    }
+
+    deleteDevice(device_id) {
+        axios.delete(`/devices/${device_id}`).then(res => {
+            this.getDevices()
+        })
+    }
+
     render() {
         const devices = this.state.devices.map((ele, index) => {
-            return <Device key={index} name={ele.name} type={ele.type}/>
+            return <div key={index}>
+                    <div>
+                        <Device name={ele.name} type={ele.type}/>
+                    </div>
+                    <div>
+                        <button onClick={() => this.deleteDevice(ele.device_id)}>Delete {ele.name}</button>
+                    </div>
+                </div>
         })
         return (
             <div>
